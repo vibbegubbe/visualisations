ce2deb5962a6d539ea2951033b432805c59f7787
vibbegubbe <villevaltteri.helminen@live.com>
2026-06-17 22:19:57 +0300

    Add files via upload

---
+++ catchments/styles/directions_2022_4_style.js
@@ -0,0 +1,41 @@
+var size = 0;
+var placement = 'point';
+function categories_directions_2022_4(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case 'inbound':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(226,226,226,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.9}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_directions_2022_4 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("dir_name");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_directions_2022_4(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
